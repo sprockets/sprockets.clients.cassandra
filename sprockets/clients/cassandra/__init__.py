4abbd63b2f05c2be82471de26cce8bf6ff8b9dc6
Brian Korty <briank@aweber.com>
2015-05-06 16:08:50 -0400

    Get default unit tests working.
    Get the default unit tests working. Fix the package directory
    structure and add the version field into the package init file.

---
+++ sprockets/clients/cassandra/__init__.py
@@ -0,0 +1,9 @@
+"""
+clients.cassandra
+=================
+
+Base functionality for accessing/modifying data in Cassandra.
+
+"""
+version_info = (0, 0, 0)
+__version__ = '.'.join(str(v) for v in version_info)
